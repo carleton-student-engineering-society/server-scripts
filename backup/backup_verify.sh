5dd2ada3582008fc06a1c9be92d459f5a0f1b858
Finlay Maroney <finman292004@protonmail.com>
2023-06-18 20:40:29 -0400

    Fixed backup verification script

--- backup/backup_verify.sh
+++ backup/backup_verify.sh
@@ -2,7 +2,7 @@
 source constants.sh
 
 FILE_COUNT=$(find $destdir -type f -mtime -0.9 -print | wc -l)
-if (( $FILE_COUNT < 3 )); then
+if (( $FILE_COUNT < 2 )); then
     echo "Backup failed, not all files are present in backup directory!" >&2
     exit 1
 fi
